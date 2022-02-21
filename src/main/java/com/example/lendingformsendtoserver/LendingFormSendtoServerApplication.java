4115c0eb9c61276083bd96c3811d1b3c276f1d4a
zaslkov <vladyslav.koshulko@ericsson.com>
2022-02-21 23:03:39 +0200

    init commit

---
+++ src/main/java/com/example/lendingformsendtoserver/LendingFormSendtoServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.lendingformsendtoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingFormSendtoServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LendingFormSendtoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lendingformsendtoserver/LendingFormSendtoServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.lendingformsendtoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingFormSendtoServerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
